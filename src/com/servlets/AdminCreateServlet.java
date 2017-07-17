b67bea4342939806b36e89c84b68f062e2efa27b
Maynard Si <jan.si1997@gmail.com>
2017-07-18 00:17:16 +0800

    Fixed CSS and links

--- src/com/servlets/AdminCreateServlet.java
+++ src/com/servlets/AdminCreateServlet.java
@@ -27,7 +27,7 @@ public class AdminCreateServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		request.getRequestDispatcher("/admincreate.jsp").forward(request, response);
+		request.getRequestDispatcher("/admin/create.jsp").forward(request, response);
 	}
 
 	/**
--- src/com/servlets/ViewUsersServlet.java
+++ src/com/servlets/ViewUsersServlet.java
@@ -27,7 +27,7 @@ public class ViewUsersServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		request.getRequestDispatcher("/viewusers.jsp").forward(request, response);
+		request.getRequestDispatcher("/admin/users.jsp").forward(request, response);
 	}
 
 	/**
