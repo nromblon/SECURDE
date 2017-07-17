ebbd9ad0a4b1b2129fbe22b8981cc9d78ee8fad3
Neil Romblon <neilromblon@live.com>
2017-07-17 20:15:35 +0800

    removed publication jsp

--- src/com/servlets/PublicationDetailsServlet.java
+++ src/com/servlets/PublicationDetailsServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class PublicationDetailsServlet
  */
-@WebServlet("/publicationdetails")
+@WebServlet("/publication/details")
 public class PublicationDetailsServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
