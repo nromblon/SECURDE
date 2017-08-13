d9808eb9d830ec3c14da47e97db77934a286052d
Neil Romblon <neilromblon@live.com>
2017-08-13 13:24:24 +0800

    added privilege constants

---
+++ src/com/constants/Privilege.java
@@ -0,0 +1,8 @@
+package com.constants;
+
+public interface Privilege {
+	public static final int USER = 1;
+	public static final int LIB_MANAGER = 2;
+	public static final int LIB_STAFF = 3;
+	public static final int ADMIN = 4;
+}
