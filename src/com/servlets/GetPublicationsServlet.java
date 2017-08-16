aa24353f56eb165c62843f6237df5956b8ed8b59
Maynard Si <jan.si1997@gmail.com>
2017-08-16 16:18:42 +0800

    login and search validation
    validation

--- src/com/servlets/GetPublicationsServlet.java
+++ src/com/servlets/GetPublicationsServlet.java
@@ -17,6 +17,7 @@ import com.db.DBConnector;
 import com.models.PublicationModel;
 import com.models.UserModel;
 import com.objects.Publication;
+import com.utils.Validator;
 
 /**
  * Servlet implementation class AddPublicationServlet
@@ -39,6 +40,26 @@ public class GetPublicationsServlet extends HttpServlet {
 		String searchTerm = request.getParameter("searchTerm");
 		String searchBy = request.getParameter("searchBy");
 		String category = request.getParameter("category");
+		
+		System.out.println(searchBy+" -- "+category);
+		
+		Validator validator = Validator.getInstance();
+		
+		if(!(validator.isAlphaNumericHasSpace(searchTerm, 45)&&
+				validator.isAlphaNumericHasSpace(searchBy,45)&&
+				validator.isAlphaNumericHasSpace(category,45))){
+			System.out.println(validator.isAlphaNumericHasSpace(searchTerm, 45));
+			System.out.println(validator.isAlphaNumericHasSpace(searchBy, 45));
+			System.out.println(validator.isAlphaNumericHasSpace(category, 45));
+			request.setAttribute("error", "Invalid Input!");
+        	request.getRequestDispatcher("/search").forward(request, response);
+        	return;
+		}
+		System.out.println("lumabas");
+		System.out.println(validator.isAlphaNumericHasSpace(searchTerm, 45));
+		System.out.println(validator.isAlphaNumericHasSpace(searchBy, 45));
+		System.out.println(validator.isAlphaNumericHasSpace(category, 45));
+		
 		ArrayList<Publication> results = PublicationModel.searchPubBy(searchTerm, searchBy, category);
 		String message = "";
 		
